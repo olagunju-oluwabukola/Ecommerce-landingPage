a31597280a6b1f52a6c2419e82d8582f9ef16762
unknown <olagunjuoluwabukola2018@gmail.com>
2024-08-27 11:25:35 +0100

    new changes

--- src/components/CarouselComp.vue
+++ src/components/CarouselComp.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="container mx-auto py-6">
-    <!-- Carousel Wrapper -->
     <div class="relative w-full overflow-hidden">
-      <!-- Carousel Items -->
       <div
         ref="carousel"
         class="flex transition-transform duration-700 ease-in-out"
         :style="{ transform: `translateX(-${currentSlide * 100 / slidesPerView}%)` }"
       >
-        <!-- Individual Slide -->
         <div
           v-for="(item, index) in items"
           :key="index"
-          class="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4"
+          class="flex-shrink-0 w-full"
+          :class="itemClass"
         >
           <div class="p-4 text-center">
-            <img :src=item.src alt="Product Image" class="w-48 h-48 mx-auto" />
+            <img :src="item.src" alt="Product Image" class="w-48 h-48 mx-auto" />
             <p class="mt-2">{{ item.name }}</p>
             <p class="mt-1 font-semibold">{{ item.price }}</p>
           </div>
         </div>
       </div>
 
-     Navigation Buttons
-       <button
+      <!-- Navigation buttons -->
+      <button
         class="absolute left-0 top-1/2 transform -translate-y-1/2 px-2 py-1 bg-gray-800 text-white rounded-full"
         @click="prevSlide"
       >
@@ -40,35 +38,45 @@
 </template>
 
 <script>
-import img1 from '/images/img1.png'
-import img2 from '/images/img2.png'
-import img3 from '/images/img3.png'
-import img4 from '/images/img4.png'
-import img5 from '/images/img5.png'
-import img6 from '/images/img6.png'
-import img7 from '/images/img7.png'
-import img8 from '/images/img8.png'
-import img9 from '/images/img9.png'
+import img1 from '/images/img1.png';
+import img2 from '/images/img2.png';
+import img3 from '/images/img3.png';
+import img4 from '/images/img4.png';
+import img5 from '/images/img5.png';
+import img6 from '/images/img6.png';
+import img7 from '/images/img7.png';
+import img8 from '/images/img8.png';
+import img9 from '/images/img9.png';
 
 export default {
   data() {
     return {
       currentSlide: 0,
-      slidesPerView: 4,
+      slidesPerView: 1, // Default to 1 slide per view on small screens
       items: [
-        { name: "Retro sneakers", price: "NGN 3000", src: img1},
-        { name: "Retro sneakers", price: "NGN 3000", src: img2},
-        { name: "Retro sneakers", price: "NGN 3000", src: img3},
-        { name: "Retro sneakers", price: "NGN 3000", src: img4},
-        { name: "Retro sneakers", price: "NGN 3000", src: img5},
-        { name: "Retro sneakers", price: "NGN 3000", src: img6},
-        { name: "Retro sneakers", price: "NGN 3000", src: img7},
-        { name: "Retro sneakers", price: "NGN 3000", src: img8},
-        { name: "Retro sneakers", price: "NGN 3000", src: img9},
-       
+        { name: "Retro sneakers", price: "NGN 3000", src: img1 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img2 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img3 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img4 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img5 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img6 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img7 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img8 },
+        { name: "Retro sneakers", price: "NGN 3000", src: img9 },
       ],
       interval: null,
     };
+  },
+  computed: {
+    itemClass() {
+      if (this.slidesPerView === 1) {
+        return "sm:w-full";
+      } else if (this.slidesPerView === 2) {
+        return "sm:w-1/2";
+      } else {
+        return "sm:w-1/4";
+      }
+    },
   },
   mounted() {
     this.updateSlidesPerView();
@@ -77,19 +85,18 @@ export default {
   },
   methods: {
     updateSlidesPerView() {
-     
       if (window.innerWidth < 640) {
-        this.slidesPerView = 1; 
+        this.slidesPerView = 1;
       } else if (window.innerWidth < 1024) {
-        this.slidesPerView = 2; 
+        this.slidesPerView = 2;
       } else {
-        this.slidesPerView = 4; 
+        this.slidesPerView = 4;
       }
     },
     startAutoSlide() {
       this.interval = setInterval(() => {
         this.nextSlide();
-      }, 3000); 
+      }, 3000);
     },
     stopAutoSlide() {
       clearInterval(this.interval);
